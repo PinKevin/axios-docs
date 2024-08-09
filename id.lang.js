1703affee70103a41b37a5ccd4e33655dc4c6d75
emerio.kevin <emerio.kevin@gmail.com>
2024-08-09 08:31:58 +0000

    feat: add id config

---
+++ id.lang.js
@@ -0,0 +1,148 @@
+/**
+ * Configuration for the Indonesian translation
+ */
+
+module.exports = {
+  display: "Bahasa Indonesia",
+  prefix: "/id/",
+  dir: "ltr",
+  lang: "id",
+  p: {
+    headline: "Klien HTTP berbasis Promise untuk peramban dan Node.js",
+    subhead: `Axios merupakan klien HTTP sederhana berbabis Promise untuk peramban dan Node.js. 
+              Axios menawarkan library yang mudah digunakan dalam package ringan dengan antarmuka
+              yang dapat diperluas.`,
+    sponsors: `<p>Terima kasih telah mendukung proyek kami.</p>
+               <p>Anda akan ditambahkan ke daftar ini secara otomatis dalam 24 jam jika tingkat yang Anda pilih memberikan manfaat ini.</p>`
+  },
+  t: {
+    "Get Started": "Mulai",
+    "View on GitHub": "Lihat di GitHub",
+    "Languages": "Bahasa",
+    "Open Source": "Sumber Terbuka",
+    "Contribute": "Berkontribusi",
+    "Source on GitHub": "Sumber di GitHub",
+    "Issues": "Isu",
+    "Pull Requests": "Pull Request",
+    "Code of Conduct": "Kode Etik",
+    "Fork on GitHub": "Fork di GitHub",
+    "Fork the Website": "Fork Situs Web",
+    "Create an Issue": "Buat Isu",
+    "Next": "Selanjutnya",
+    "Previous": "Sebelumnya",
+    "Website Copy Right Footer": "Catatan Kaki Hak Cipta Situs Web",
+    "View On Github": "Lihat di GitHub",
+    "Axios Project Copy Right Footer": "Catatan Kaki Hak Cipta Proyek Axios",
+    "License Label Footer": "Catatan Kaki Label Lisensi",
+    "Sponsors": "Sponsor",
+    "Become a sponsor": "Jadi sponsor"
+  },
+  sidebar: [
+    {
+      type: "heading",
+      text: "Memulai",
+    },
+    {
+      type: "link",
+      href: "/docs/intro",
+      text: "Pengantar",
+    },
+    {
+      type: "link",
+      href: "/docs/example",
+      text: "Contoh",
+    },
+    {
+      type: "link",
+      href: "/docs/post_example",
+      text: "Permintaan POST",
+    },
+    {
+      type: "heading",
+      text: "API AXIOS",
+    },
+    {
+      type: "link",
+      href: "/docs/api_intro",
+      text: "API Axios",
+    },
+    {
+      type: "link",
+      href: "/docs/instance",
+      text: "Instansi Axios",
+    },
+    {
+      type: "link",
+      href: "/docs/req_config",
+      text: "Konfigurasi Permintaan",
+    },
+    {
+      type: "link",
+      href: "/docs/res_schema",
+      text: "Skema Permintaan",
+    },
+    {
+      type: "link",
+      href: "/docs/config_defaults",
+      text: "Konfigurasi Bawaan",
+    },
+    {
+      type: "link",
+      href: "/docs/interceptors",
+      text: "Pencegat",
+    },
+    {
+      type: "link",
+      href: "/docs/handling_errors",
+      text: "Penanganan Galat",
+    },
+    {
+      type: "link",
+      href: "/docs/cancellation",
+      text: "Pembatalan",
+    },
+    {
+      type: "link",
+      href: "/docs/urlencoded",
+      text: "🆕 Isi URL-Encoding",
+    },
+    {
+      type: "link",
+      href: "/docs/multipart",
+      text: "🆕 Isi Multipart",
+    },
+    {
+      type: "heading",
+      text: "Lainnya",
+    },
+    {
+      type: "link",
+      href: "/docs/notes",
+      text: "Catatan",
+    },
+    {
+      type: "heading",
+      text: "Kontributor",
+    },
+    {
+      type: "link",
+      href: "https://github.com/axios/axios/blob/master/CODE_OF_CONDUCT.md",
+      text: "Kode Etik",
+    },
+    {
+      type: "link",
+      href: "https://github.com/axios/axios/blob/master/COLLABORATOR_GUIDE.md",
+      text: "Panduan Kolaborator",
+    },
+    {
+      type: "link",
+      href: "https://github.com/axios/axios/blob/master/CONTRIBUTING.md",
+      text: "Berkontribusi di Axios"
+    },
+    {
+      type: "link",
+      href: "/docs/translating",
+      text: "Menerjemahkan dokumentasi ini"
+    }
+  ],
+};
